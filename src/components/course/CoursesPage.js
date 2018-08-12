69b10b8734a34ff394dec42d2ca0b8ea09b0ff02
Albertly Lyubarsky <albertly@comtec.co.il>
2018-08-12 17:49:26 +0300

    wiring redux into form

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -13,28 +13,36 @@ class CoursesPage extends React.Component {
         };
 
         this.onTitleChange = this.onTitleChange.bind(this);
-       // this.onClickSave = this.onClickSave.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
 
-       this.onClickSave = (event) => { 
-           alert(`Saving ${this.state.course.title}`);
-           this.props.dispatch(courseActions.createCourse(this.state.course));
+        this.setStateValues = value => {
+            const course = this.state.course;
+            course.title = value;
+            this.setState({
+                course: course
+            });    
         };
     }
 
-    onTitleChange(event) {
-        const course = this.state.course;
-        course.title = event.target.value;
-        this.setState({
-            course: course
-        });
+    onClickSave()  { 
+        //alert(`Saving ${this.state.course.title}`);
+        this.props.createCourse(this.state.course);
+        this.setStateValues("");
     }
 
-    
+    onTitleChange(event) {
+        this.setStateValues(event.target.value);
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
+    }
 
     render() {
         return ( 
             <div>
                 <h1> Courses </h1> 
+                {this.props.courses.map(this.courseRow)}
                 <h2>Add Course</h2>
                     <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
                     <input type="submit" value="Save" onClick={this.onClickSave} />           
@@ -43,10 +51,20 @@ class CoursesPage extends React.Component {
     }
 }
 
+CoursesPage.propTypes = {
+    courses: PropTypes.array.isRequired,
+    createCourse: PropTypes.func.isRequired
+};
+
 function mapStateToProps(state, ownProps) {
     return {
         courses: state.courses
     };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+    return {
+        createCourse: course => dispatch(courseActions.createCourse(course))
+    };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
