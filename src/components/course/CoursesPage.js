2af4383a2a30be92dbaac6abfd0fb389f655dd7c
Albert Lyubarsky <Albert@COM-LT062.comtec.co.il>
2018-08-14 05:18:32 +0300

    Adding Course List

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { PropTypes } from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
     constructor(props, context) {
@@ -46,10 +47,12 @@ class CoursesPage extends React.Component {
     }
 
     render() {
+        const {courses} = this.props;
+
         return ( 
             <div>
                 <h1> Courses </h1> 
-                {this.props.courses.map(this.courseRow)}
+                <CourseList courses={courses} />
                 <h2>Add Course</h2>
                     <input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
                     <input type="submit" value="Save" onClick={this.onClickSave} />           
